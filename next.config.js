2b813ec940e6e8d95dee1930f1aaf5dec33a07bd
Charalampos Fanoulis <yo@fanoulis.dev>
2023-01-03 22:09:52 +0200

    feat: site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bank.hackclub.com",
+        pathname: "/storage/blobs/redirect/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

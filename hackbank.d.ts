7fe9cdd0a619852bd74dd1cf709b632cb3cb7a9a
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2024-04-25 11:05:41 +0000

    feat: filter projects

--- hackbank.d.ts
+++ hackbank.d.ts
@@ -1,6 +1,8 @@
 export interface BankOrg {
   id: string;
   name: string;
+  category: string;
   slug: string;
   logo: string;
+  demo_mode: boolean;
 }
--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,13 +1,17 @@
 import type { BankOrg } from '../hackbank';
 
-export async function getBankData(page: number, page_size = 50) {
+const allowedProjectTypes = ["hackathon", "hackclub", "nonprofit", "event", "high_school_hackathon", "robotics_team", "hack_club_hq", "ai"];
+
+export async function getBankData(page: number, page_size = 50, showAll = false) {
 	const getOrgsReq = await fetch(
 		`https://bank.hackclub.com/api/v3/organizations?page=${page}&per_page=${page_size}&suppose_this_is_a_useragent=diafano_by_charalampos`
 	);
 
 	if (!getOrgsReq.ok) throw "Couldn't fetch orgs from da hack bank";
 
-	const data = ((await getOrgsReq.json()) as BankOrg[]).map(({ id, name, slug, logo }) => ({
+	const data = ((await getOrgsReq.json()) as BankOrg[])
+		.filter(e => showAll || (allowedProjectTypes.includes(e.category) && !e.demo_mode))
+		.map(({ id, name, slug, logo }) => ({
 		id,
 		name,
 		slug,

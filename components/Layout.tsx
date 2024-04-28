7ed4043588a059254ed552fc54afa6f445eb35dc
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2024-04-28 18:42:08 +0000

    fix: whos bank

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import styles from '../styles/index.module.css';
+import { PropsWithChildren } from 'react';
 
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default function Layout({children}: PropsWithChildren<{}>) {
 	return (
 		<>
 		<Head>
 			<title>Diafano - HCB&apos;s directory</title>
-			<meta name="description" content="A directory of all Hack Club Bank projects that have enabled super-transparency" />
+			<meta name="description" content="A directory of all Hack Club HCB projects that have enabled super-transparency" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<link rel="icon" href="/favicon.png" />
 		</Head>
 		<main className={styles.main}>
 			<h1 className={`${styles.title} title`}>
 				These cool projects use{' '}
-				<a className={styles.gradient} href="https://hackclub.com/bank?ref=diafano">
+				<a className={styles.gradient} href="https://hackclub.com/hcb?ref=diafano">
 					Hack Club Bank
 				</a>
 				:
